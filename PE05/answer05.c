254a66d9e49d3042b1c40f59b7c8e34d0d9c6f4d
mmalesev <mmalesev@purdue.edu>
2015-09-30 19:19:30 -0400

    PE05 done

--- PE05/answer05.c
+++ PE05/answer05.c
@@ -13,32 +13,30 @@ void logFibonacci(int n, unsigned long *fn1, unsigned long *fn2)
   // increment the recursion counter 
 
   Increment_counter(&n);
-
+   
   // terminating condition
-
-
-
-
-
-  // recursion
-  // you have to halve n, 
-  // depending on whether n/2 is even or odd, recursively
-  // call logFibonacci with appropriate parameters
-
-
-
-
-
-
-
-
-
-
-
-
-
-   // decrement counter after all recursive calls
-   // and before returning from the function.
+  if (n == 2) {
+      *fn1 = 1;
+      *fn2 = 1;
+  }
+  else{
+      long x;
+      long y;
+      long temp;
+      if ((n / 2) % 2 == 0){
+          logFibonacci(n / 2, fn1, fn2);
+      }
+      else{
+          logFibonacci(n / 2 + 1, fn1, fn2);
+          temp = *fn1;
+          *fn1 = *fn2;
+          *fn2 = temp - *fn2;
+      }
+      x = *fn1;
+      y = *fn2;
+      *fn1 = 2 * y * x + x * x;
+      *fn2 = y * y + x * x;
+  }
 
    Decrement_counter();
 }
@@ -54,23 +52,23 @@ unsigned long Fibonacci(int n)
 {
    /* for storing the results when calling logFibonacci */
 
-   unsigned long fn1 = 0; 
-   unsigned long fn2 = 0;
+   unsigned long fn1 = 0; //F(n) 
+   unsigned long fn2 = 0; //F(n-1)
 
    /* simple cases */
-
-
+   if (n == 0) {return 0;}
+   if (n == 1) {return 1;}
 
    /* if even n, call logFibonacci with n and return fn1 */
-   /* if odd n, call with n-1 or n+1, combine fn1 and fn2 as F(n) */
-
-
-
-
-
-   return fn1 + fn2;  // should change this, doing this so that compiler 
-                      // would not complain.
-
+   /* if odd n, call with n-1, and then combine fn1 and fn2 as F(n) */
+   if (n % 2 == 0){
+       logFibonacci(n, &fn1, &fn2);
+       return fn1;
+   }
+   else{
+       logFibonacci(n - 1, &fn1, &fn2);
+       return fn1 + fn2;
+   }
 }
 
 #endif
