e659d0d3fa1cbcea6953a5cc86cf74c48c7e8a2c
mmalesev <mmalesev@purdue.edu>
2015-11-06 21:33:44 -0500

    everything works instead of airplane

--- PE09/bmp.c
+++ PE09/bmp.c
@@ -60,16 +60,29 @@ int Is_BMP_Header_Valid(BMP_Header* header, FILE *fptr) {
 
   // fill in extra to check for file size, image size
   // based on bits, width, and height
+  fseek(fptr, 0, SEEK_END);
+  if(header->size != ftell(fptr)){
+    return FALSE;
+  }
 
+  if(header->size != header-> imagesize + sizeof(*header)){
+     return FALSE;
+  }
+  
+  int padding;
 
-
-
-
-
-
-
-
-
+  if(header->bits == 24){
+    padding = (4 - ((header->width * 3) % 4)) % 4;
+    if(header->imagesize != (header->width * 3 + padding) * header->height){
+      return FALSE;
+    }
+  }
+  else{
+    padding = header->width % 2 * 2;
+    if(header->imagesize != (header->width * 2 + padding) * header->height){
+      return FALSE;
+    }
+  }
 
   return TRUE;
 }
@@ -85,33 +98,42 @@ int Is_BMP_Header_Valid(BMP_Header* header, FILE *fptr) {
 BMP_Image *Read_BMP_Image(FILE* fptr) {
 
   // go to the beginning of the file
-
    BMP_Image *bmp_image = NULL;
+   fseek(fptr, 0, SEEK_SET);
 
   //Allocate memory for BMP_Image*;
-
-
-
-
+   bmp_image = malloc(sizeof(*bmp_image));
+   if(bmp_image == NULL){
+      return NULL;
+   }
   //Read the first 54 bytes of the source into the header
-
-
-
-
+   int status = fread(&(bmp_image -> header), 54, 1, fptr);
+   if(status != 1){
+      free(bmp_image);
+      return NULL;
+   }
 
   // if read successful, check validity of header
-
-
-
-
+   if(!Is_BMP_Header_Valid(&(bmp_image -> header), fptr)){
+      free(bmp_image);
+      return NULL;
+   }
 
   // Allocate memory for image data
-
-
-
-
+   bmp_image -> data = malloc(bmp_image -> header.imagesize); 
+   if(bmp_image -> data == NULL){
+      free(bmp_image);
+      return NULL;
+   }
  
   // read in the image data
+   fseek(fptr, 54, SEEK_SET);
+   status = fread(bmp_image -> data, bmp_image -> header.imagesize, 1, fptr);
+   if(status != 1){
+      free(bmp_image -> data);
+      free(bmp_image);
+      return NULL;
+   }
 
   return bmp_image;
 }
@@ -124,12 +146,19 @@ BMP_Image *Read_BMP_Image(FILE* fptr) {
 int Write_BMP_Image(FILE* fptr, BMP_Image* image) 
 {
    // go to the beginning of the file
+   fseek(fptr, 0, SEEK_SET);
 
    // write header
-
-
+   int size = fwrite(&(image -> header), 54, 1, fptr);
+   if(size != 1){
+     return FALSE;
+   }
    // write image data
-
+   size = fwrite((image -> data), image->header.imagesize, 1, fptr);
+   if(size != 1){
+     return FALSE;
+   }
+  
    return TRUE;
 }
 
@@ -137,10 +166,8 @@ int Write_BMP_Image(FILE* fptr, BMP_Image* image)
  * the BMP_Image.
  */
 void Free_BMP_Image(BMP_Image* image) {
-
-
-
-
+   free(image->data);
+   free(image);
 }
 
 // Given a valid BMP_Image, create a new image that retains the image in the
@@ -151,11 +178,94 @@ BMP_Image *Crop_BMP_Image(BMP_Image *image, int left_x,
                           int top_y)
 {
    // getting the box coordinates to stay within range
+   if(left_x < 0){
+     left_x = 0;
+   }
+   if(right_x < 0){
+     right_x = 0;
+   }
+   if(bottom_y < 0){
+     bottom_y = 0;
+   }
+   if(top_y < 0){
+     top_y = 0;
+   }
+
+   if(left_x > image->header.width - 1){
+     left_x = image->header.width - 1;
+   }
+   if(right_x > image->header.width - 1){
+     right_x = image->header.width - 1;
+   }
+   if(bottom_y > image->header.height - 1){
+     bottom_y = image->header.height - 1;
+   }
+   if(top_y > image->header.height - 1){
+     top_y = image->header.height - 1;
+   }
 
    // proceed only if left_x <= right_x and bottom_y <= top_y
+   if(left_x > right_x || bottom_y > top_y){
+     return NULL;
+   }
 
    // create a new image cropped from the given image
    BMP_Image *t_image = NULL;
+   t_image = malloc(sizeof(*t_image));
+   if(t_image == NULL){
+     return NULL;
+   }
+   t_image->header = image->header;
+   int width = right_x - left_x + 1;
+   int height = top_y - bottom_y + 1;
+
+   t_image->header.width = width;
+   t_image->header.height = height;
+
+   int padding;
+
+   if(t_image->header.bits == 24){
+     padding = (4 - ((t_image->header.width * 3) % 4)) % 4;
+     t_image->header.imagesize = (width * 3 + padding) * height;
+   }
+   else{
+     padding = t_image->header.width % 2 * 2;
+     t_image->header.imagesize = (width * 2 + padding) * height;
+   }
+   t_image->header.size = t_image->header.imagesize + 54;
+   
+   t_image->data = malloc(t_image->header.imagesize);
+   if(t_image->data == NULL){
+     return NULL;
+   }
+   /*int im_padding;
+   if(image->header.bits == 24){
+     im_padding = (4 - ((image->header.width * 3) % 4)) % 4;
+   }
+   else{
+     im_padding = image->header.width % 2 * 2;
+   }*/
+   int i, j, p;
+   long int counter = 0;
+   //int im_row = image->header.height;
+   //int im_col = image->header.width;
+   int n;
+   if(t_image->header.bits == 24){
+     n = 3;
+   }
+   else{
+     n = 2;
+   }
+   for(i = 0; i < height; i++){
+     for(j = 0; j < width * n; j++){
+       t_image->data[counter] = image->data[(i + bottom_y) * (image->header.imagesize / image->header.height) + j + left_x];
+       counter++;
+     }
+     for(p = 0; p < padding; p++){
+       t_image->data[counter] = 0;
+       counter++;
+     }
+   }
 
    return t_image;
 }
---
+++ PE09/pe09.c
@@ -0,0 +1,96 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include "bmp.h"
+#include<string.h>
+#include<limits.h>
+
+int main(int argc, char **argv){
+
+  if(argc < 3){
+    fprintf(stderr, "Not enough arguments\n");
+    return EXIT_FAILURE;
+  }
+  int i;
+  int left_x = INT_MIN;
+  int bottom_y = INT_MIN;
+  int right_x = INT_MAX;
+  int top_y = INT_MAX;
+  for(i = 1; i < argc - 1; i++){
+    if(strcmp(argv[i], "-l") == 0){
+      if((strcmp(argv[i + 1], "-r") == 0) || (strcmp(argv[i + 1], "-b") == 0) || (strcmp(argv[i + 1], "-t") == 0)){
+        fprintf(stderr, "Invalid arguments\n");
+        return EXIT_FAILURE;
+      }
+      left_x = atoi(argv[++i]);
+    }
+    if(strcmp(argv[i], "-r") == 0){
+      if((strcmp(argv[i + 1], "-l") == 0) || (strcmp(argv[i + 1], "-b") == 0) || (strcmp(argv[i + 1], "-t") == 0)){
+        fprintf(stderr, "Invalid arguments\n");
+        return EXIT_FAILURE;
+      }
+      right_x = atoi(argv[++i]);
+    }
+    if(strcmp(argv[i], "-b") == 0){
+      if((strcmp(argv[i + 1], "-l") == 0) || (strcmp(argv[i + 1], "-r") == 0) || (strcmp(argv[i + 1], "-t") == 0)){
+        fprintf(stderr, "Invalid arguments\n");
+        return EXIT_FAILURE;
+      }
+      bottom_y = atoi(argv[++i]);
+    }
+    if(strcmp(argv[i], "-t") == 0){
+      if((strcmp(argv[i + 1], "-l") == 0) || (strcmp(argv[i + 1], "-b") == 0) || (strcmp(argv[i + 1], "-r") == 0)){
+        fprintf(stderr, "Invalid arguments\n");
+        return EXIT_FAILURE;
+      }
+      top_y = atoi(argv[++i]);
+    }
+  }
+
+  char *imagefile = argv[i - 1];
+  char *outputfile = argv[i++];
+
+  BMP_Image *image;
+  BMP_Image *c_image;
+  FILE *fptr = fopen(imagefile, "rb");
+  if(fptr == NULL){
+    fprintf(stderr, "Invalid image file\n");
+    return EXIT_FAILURE;
+  }
+  FILE *fptrw = fopen(outputfile, "wb");
+  if(fptrw == NULL){
+    fprintf(stderr, "Could not open file for writing\n");
+    fclose(fptr);
+    return EXIT_FAILURE;
+  }
+  image = Read_BMP_Image(fptr);
+  if(image == NULL){
+    fprintf(stderr, "Can't read image from file\n");
+    fclose(fptr);
+    fclose(fptrw);
+    return EXIT_FAILURE;
+  }
+
+  c_image = Crop_BMP_Image(image, left_x, right_x, bottom_y, top_y);
+  if(c_image == NULL){
+    fprintf(stderr, "Can't crop the image\n");
+    Free_BMP_Image(image);
+    fclose(fptr);
+    fclose(fptrw);
+    return EXIT_FAILURE;
+  }
+
+  int check = Write_BMP_Image(fptrw, c_image);
+  if (check == FALSE){
+    fprintf(stderr, "Error writing to the output file\n");
+    Free_BMP_Image(c_image);
+    Free_BMP_Image(image);
+    fclose(fptr);
+    fclose(fptrw);
+  }
+
+  Free_BMP_Image(image);
+  Free_BMP_Image(c_image);
+  fclose(fptr);
+  fclose(fptrw);
+  return EXIT_SUCCESS;
+}
